06cb234eba025dc465f0cd90a8f4b05b97cd47f8
Christian Hunziker <ch.hunziker@gmail.com>
2023-11-11 12:37:51 +0100

    removed authguard on default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,8 +12,7 @@ const routes: Routes = [
   },
   {
     path: 'list',
-    loadChildren: () => import('./list/list.module').then(m => m.ListModule),
-    canActivate: [authenticationGuard()]
+    loadChildren: () => import('./list/list.module').then(m => m.ListModule)
   },
   {
     path: 'item/:listId/:docId',
